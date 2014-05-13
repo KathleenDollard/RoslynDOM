17631e5cc92e5d039a04ea5aae64ffd01062c91d
Kathleen Dollard <kathleen@mvps.org>
2014-05-13 10:32:46 -0600

    Setup for NuGet

--- CreateNuGet.cmd
+++ CreateNuGet.cmd
@@ -1,4 +1,4 @@
-set version=1.0.0
+set version=1.0.0-alpha
 if not exist .\nuget_packages mkdir nuget_packages
 del /Q .\nuget_packages\*.*
 .nuget\NuGet.exe pack RoslynK\RoslynK.csproj -OutputDirectory .\nuget_packages -Version %version% -symbols
